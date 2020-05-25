3f7328d903f7b6bc3a48db64df6d8d1f59d01603
Henrique <rktinelli@gmail.com>
2020-05-25 19:42:44 +0100

    Adding Multiple entries to the table

--- main_trial.py
+++ main_trial.py
@@ -15,9 +15,30 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS Person "
                  "(name VARCHAR(50), age smallint UNSIGNED,"
                  "personID int PRIMARY KEY AUTO_INCREMENT)")
 
+# Not following best practices for multiple entries
+'''
 mycursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)", ("Tim", 19))
 mycursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)", ("Joe", 22))
 mycursor.execute("INSERT INTO Person (name, age) VALUES (%s,%s)", ("Henry", 28))
+db.commit()
+'''
+
+# Adding multiple entries
+sql = "INSERT INTO Person (name, age) VALUES (%s, %s)"
+val = [
+  ('Peter', 15),
+  ('Amy', 45),
+  ('Hannah', 36),
+  ('Michael', 24),
+  ('Sandy', 15),
+  ('Betty', 5),
+  ('Richard', 55),
+  ('Susan', 49),
+  ('Vicky', 42),
+  ('Ben', 65)
+]
+mycursor.executemany(sql, val)
+db.commit()
 
 mycursor.execute("SELECT * FROM Person")
 for x in mycursor:
